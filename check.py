980251f539fcacca824fa00d5cdd35e893b2acc6
imran Niaz <103443033+imranniaz-st@users.noreply.github.com>
2023-12-04 18:38:43 +0500

    Update check.py

--- check.py
+++ check.py
@@ -1,18 +1,23 @@
 import requests
-with open('urls.txt', 'r') as f :
- for line in f :
-   print(line, end ='@')
-#try :
-   r = requests.get(line)
-   if r.status_code == 200:
-    print('200', '@Success!')
-   elif r.status_code == 301:
-    print('300', '@Moved Permanently!')
-   elif r.status_code == 404:
-    print('404', '@Not Found!')
-   elif r.status_code == 302:
-    print('302', '@Object moved!')
-   elif r.status_code == 403:
-    print('403', '@Forbidden!')
-#  print(r.status_code)
-#except requests.ConnectionError:
+
+with open('urls.txt', 'r') as f:
+    for line in f:
+        url = line.strip()  # Remove leading/trailing whitespaces, newlines, etc.
+        print(url, end=' @ ')
+
+        try:
+            r = requests.get(url)
+            if r.status_code == 200:
+                print('200 @ Success!')
+            elif r.status_code == 301:
+                print('301 @ Moved Permanently!')
+            elif r.status_code == 404:
+                print('404 @ Not Found!')
+            elif r.status_code == 302:
+                print('302 @ Object Moved!')
+            elif r.status_code == 403:
+                print('403 @ Forbidden!')
+            else:
+                print(f'{r.status_code} @ Unknown Status')
+        except requests.ConnectionError:
+            print('Connection Error')
